7cea3abeb5fbb9bb8b8a5276747e2cb4358519cf
Reegan Alward <reegan.alward@investec.co.za>
2024-05-10 16:14:06 +0200

    Added result page and requirements

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def predict():
     input_seven = request.form['7']
     input_eight = request.form['8']
     
-    setup_df = pd.DataFrame([pd.series([input_one, input_two, input_three, input_four, input_five, input_six, input_seven, input_eight])])
+    setup_df = pd.DataFrame([pd.Series([input_one, input_two, input_three, input_four, input_five, input_six, input_seven, input_eight])])
     diabetes_prediction = model.predict_proba(setup_df)
     output = '{0:.{1}f}'.format(diabetes_prediction[0][1], 2)
     output = str(float(output)*100)+'%'
